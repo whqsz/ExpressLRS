6e3bce614be3167da629dffe87971bea9cd8e65a
Johannes Kasberger <schugabe@gmx.at>
2020-10-10 00:44:58 +0200

    add telemetry receive code to tx

--- src/src/tx_main.cpp
+++ src/src/tx_main.cpp
@@ -26,6 +26,7 @@ SX1280Driver Radio;
 #include "hwTimer.h"
 #include "LQCALC.h"
 #include "LowPassFilter.h"
+#include <stubborn_link.h>
 
 #ifdef PLATFORM_ESP8266
 #include "soc/soc.h"
@@ -112,6 +113,10 @@ void EnterBindingMode();
 void ExitBindingMode();
 void SendUIDOverMSP();
 
+StubbornLink telemetryInLink;
+uint8_t CRSFinBuffer[CRSF_MAX_PACKET_LEN];
+volatile bool TelemetryConfirm = false;
+
 // MSP packet handling function defs
 void ProcessMSPPacket(mspPacket_t *packet);
 void OnRFModePacket(mspPacket_t *packet);
@@ -177,7 +182,17 @@ void ICACHE_RAM_ATTR ProcessTLMpacket()
     //crsf.LinkStatistics.downlink_Link_quality = Radio.currPWR;
     crsf.LinkStatistics.rf_Mode = 4 - ExpressLRS_currAirRate_Modparams->index;
 
-    crsf.TLMbattSensor.voltage = (Radio.RXdataBuffer[3] << 8) + Radio.RXdataBuffer[6];
+    crsf.sendLinkStatisticsToTX();
+    if (telemetryInLink.ReceiveData(Radio.RXdataBuffer[3], Radio.RXdataBuffer[6]))
+    {
+        // flip bit in radio message
+        TelemetryConfirm = !TelemetryConfirm;
+    }
+    uint8_t receivedLength = telemetryInLink.GetReceivedData();
+    if (receivedLength > 0)
+    {
+        crsf.sendTelemetryToTX(receivedLength, CRSFinBuffer);
+    }
   }
 }
 
@@ -382,7 +397,7 @@ void ICACHE_RAM_ATTR SendRCdataToRF()
     else
     {
       #if defined HYBRID_SWITCHES_8
-      GenerateChannelDataHybridSwitch8(Radio.TXdataBuffer, &crsf, DeviceAddr);
+      GenerateChannelDataHybridSwitch8(Radio.TXdataBuffer, &crsf, DeviceAddr, TelemetryConfirm);
       #elif defined SEQ_SWITCHES
       GenerateChannelDataSeqSwitch(Radio.TXdataBuffer, &crsf, DeviceAddr);
       #else
@@ -681,6 +696,8 @@ void setup()
     delay(1000);
     #endif
   }
+  telemetryInLink.ResetState();
+  telemetryInLink.SetDataToReceive(sizeof(CRSFinBuffer), CRSFinBuffer);
   POWERMGNT.setDefaultPower();
 
   eeprom.Begin(); // Init the eeprom
